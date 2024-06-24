2124c0713407e39f0aced0132f8aee4afa923dbf
Pavlo Myrskyi <pavlo.myrskyi@games>
2024-06-24 02:54:33 +0200

    added game constants

---
+++ Assets/_Project/Scripts/GameConstants/SceneLibraryConstants.cs
@@ -0,0 +1,8 @@
+namespace _Project.GameConstants
+{
+	public enum SceneLibraryConstants : byte
+	{
+		PRELOADER = 0,
+		GAMEPLAY = 1
+	}
+}
---
+++ Assets/_Project/Scripts/GameConstants/WindowsConstants.cs
@@ -0,0 +1,10 @@
+namespace _Project.GameConstants
+{
+    public static class WindowsConstants
+    {
+        public static string MAIN_UI_WINDOW = "MainUIWindow";
+        public const string LOADING_WINDOW = "LoadingWindow";
+        public const string HUD_WINDOW = "HUDWindow";
+        public const string LEADERBOARD_WINDOW = "LeaderboardWindow";
+    }
+}
